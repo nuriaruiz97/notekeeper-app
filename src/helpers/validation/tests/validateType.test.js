051070d195367d1970e1d83bf3bb3425e85d3446
Núria Ruiz Font <nuriaruizfont@gmail.com>
2024-05-13 20:21:12 +0200

    try to fix lint & lint:fix

--- src/helpers/validation/tests/validateType.test.js
+++ src/helpers/validation/tests/validateType.test.js
@@ -1,8 +1,8 @@
-import { validateType } from "../validateType";
-import { describe, test, expect } from "vitest";
+import { validateType } from "../validateType.js";
+import { describe, test, expect, it } from "vitest";
 
 describe("Given a validateType function", () => {
-  describe("When it receives a note object with properties that match the expected data types", () => {
+  describe("When it receives a object with properties that match the expected data types", () => {
     test("Then it should return true", () => {
       const note = { title: "Title", status: "done" };
       const expectedTypes = { title: "string", status: "string" };
@@ -10,11 +10,60 @@ describe("Given a validateType function", () => {
       expect(result).toBe(true);
     });
   });
-
-  describe("When it receives a note object with properties that do not match the expected data types", () => {
+  describe("When it receives a object with properties that do not match the expected data types", () => {
     test("Then it should return false", () => {
       const note = { title: "Title", status: "done" };
       const expectedTypes = { title: "string", status: "number" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(false);
+    });
+  });
+});
+describe("Given a validateType function", () => {
+  describe("given type string", () => {
+    it("should return true if the property is a string", () => {
+      const note = { title: "Title" };
+
+      const expectedTypes = { title: "string" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(true);
+    });
+    it("should return false if the property is not a string", () => {
+      const note = { title: 1 };
+
+      const expectedTypes = { title: "string" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(false);
+    });
+  });
+  describe("given type number", () => {
+    it("should return true if the property is a number", () => {
+      const note = { id: 1 };
+
+      const expectedTypes = { id: "number" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(true);
+    });
+    it("should return false if the property is not a number", () => {
+      const note = { id: "1" };
+
+      const expectedTypes = { id: "number" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(false);
+    });
+  });
+  describe("given type boolean", () => {
+    it("should return true if the property is a boolean", () => {
+      const note = { isDone: true };
+
+      const expectedTypes = { isDone: "boolean" };
+      const result = validateType(note, expectedTypes);
+      expect(result).toBe(true);
+    });
+    it("should return false if the property is not a boolean", () => {
+      const note = { isDone: "true" };
+
+      const expectedTypes = { isDone: "boolean" };
       const result = validateType(note, expectedTypes);
       expect(result).toBe(false);
     });
