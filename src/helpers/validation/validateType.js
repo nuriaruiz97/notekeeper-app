ed98476bface2db0c58c32ab8229198960c5836c
Núria Ruiz Font <nuriaruizfont@gmail.com>
2024-05-13 19:09:44 +0200

    started validators, need to create new branch

---
+++ src/helpers/validation/validateType.js
@@ -0,0 +1,13 @@
+/**
+ * Validates the data types of properties in a note object.
+ * @param {object} note - The object to validate.
+ * @param {object} expectedTypes - Specifying the expected data types for each property.
+ * @returns {boolean} - True if all properties match their expected types, false otherwise.
+ */
+function validateType(note, expectedTypes) {
+  const typeMatches = Object.keys(expectedTypes).map((key) => {
+    return note.hasOwnProperty(key) && typeof note[key] === expectedTypes[key];
+  });
+  return typeMatches.every((match) => match === true);
+}
+export default validateType;
